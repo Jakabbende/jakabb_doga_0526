61fcc77f4eab3cdce15edd2584685ac8e7ecd926
JakabBende <jakabbende1014@gmail.com>
2025-05-26 08:22:53 +0200

    vege

--- jakabb_doga_0526/MainWindow.xaml.cs
+++ jakabb_doga_0526/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Pqc.Crypto.Lms;
 
 namespace jakabb_doga_0526
 {
@@ -20,9 +22,46 @@ namespace jakabb_doga_0526
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        MySqlConnection kapcsolat = new MySqlConnection("server = localhost;database = jakabb_11a; uid = 'root'; password = ''");
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            kapcsolat.Open();
+            var lek = new MySqlCommand("SELECT * FROM filmek", kapcsolat).ExecuteReader();
+            lbadatok.Items.Clear(); 
+            while (lek.Read())
+            {
+                lbadatok.Items.Add(lek["filmazon"] + ";" + lek["cim"] + ";" + lek["ev"] + ";" + lek["szines"] + ";" + lek["mufaj"] + ";" + lek["hossz"]);
+            }
+            kapcsolat.Close();
+        }
+
+        private void lbadatok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbadatok.SelectedItem == null)
+                return;
+
+            var valasztott = lbadatok.SelectedItem.ToString();
+            var adatok = valasztott.Split(';');
+            lbfilmazon.Content = adatok[0];
+            tb1.Text = adatok[1];
+            tb2.Text = adatok[2];
+            tb3.Text = adatok[3];
+            tb4.Text = adatok[4];
+            tb5.Text = adatok[5];
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            kapcsolat.Open();
+            var upd = new MySqlCommand($"update filmek set cim = '{tb1.Text}',ev = {tb2.Text},szines = '{tb3.Text}',mufaj = '{tb4.Text}', hossz = {tb5.Text} where filmazon = '{lbfilmazon.Content}'", kapcsolat).ExecuteNonQuery();
+            kapcsolat.Close();
         }
     }
 }
